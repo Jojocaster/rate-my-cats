419193f19031e5c3d153f00ef89040edbc5e6ffb
jojocaster <=>
2026-06-07 22:30:43 +0100

    tests fixed

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
-import "./unistyles";
-
 jest.mock("react-native/Libraries/EventEmitter/NativeEventEmitter");
 
 jest.mock("react-native-nitro-modules", () => ({}));
+
+jest.mock('react-native-worklets', () =>
+  require('react-native-worklets/lib/module/mock')
+);
